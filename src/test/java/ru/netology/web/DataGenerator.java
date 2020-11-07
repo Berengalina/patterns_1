59e5d8f0a1211eece8819b0ac7bc845ae498d4f2
Anna Belousova <annabelousovva@gmail.com>
2020-11-07 23:08:50 +0300

    Update Tests

--- src/test/java/ru/netology/web/DataGenerator.java
+++ src/test/java/ru/netology/web/DataGenerator.java
@@ -1,17 +1,58 @@
 package ru.netology.web;
 
 import com.github.javafaker.Faker;
-import lombok.Data;
-import lombok.RequiredArgsConstructor;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Locale;
+import java.util.Random;
 
-@Data
-@RequiredArgsConstructor
 
 public class DataGenerator {
-    Faker faker = new Faker(new Locale("ru"));
-    private final String city = faker.address().city();
-    private final String name = faker.name().fullName();
-    private final String phoneNumber = faker.phoneNumber().phoneNumber();
+    private DataGenerator() {
+    }
+
+    public static class Registration {
+        private Registration() {
+        }
+
+
+        public static String generateByFakerName(String locale) {
+            Faker faker = new Faker(new Locale("ru"));
+            return new String(
+                    faker.name().fullName()
+            );
+        }
+
+
+        public static String generateByFakerPhone(String locale) {
+            Faker faker = new Faker(new Locale("ru"));
+            return new String(
+                    faker.phoneNumber().phoneNumber()
+            );
+        }
+
+
+        public static String generateFirstDateOfMeeting() {
+            String firstDate = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+            return firstDate;
+        }
+
+
+        public static String generateSecondDateOfMeeting() {
+            String secondDate = LocalDate.now().plusDays(4).format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+            return secondDate;
+        }
+
+
+        public static String generateCity() {
+            String[] adminCities = new String[]{"Москва", "Санкт-Петербург", "Калуга", "Владимир", "Краснодар", "Майкоп", "Уфа", "Пермь",
+                    "Владивосток", "Рязань", "Самара", "Курск", "Брянск", "Казань", "Хабаровск", "Волгоград", "Иваново", "Вологда",
+                    "Чита", "Барнаул", "Иркутск", "Якутск", "Улан-Удэ", "Петрозаводск", "Липецк"};
+            Random generator = new Random();
+            int randomIndex = generator.nextInt(adminCities.length);
+            return adminCities[randomIndex];
+        }
+
+    }
 }
--- src/test/java/ru/netology/web/RegistrationTest.java
+++ src/test/java/ru/netology/web/RegistrationTest.java
@@ -3,34 +3,28 @@ package ru.netology.web;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
-
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
-public class RegistrationTest {
 
-    DataGenerator dataGenerator = new DataGenerator();
+public class RegistrationTest {
 
     @Test
     void shouldRegisterCardDelivery() {
         open("http://localhost:9999");
-        $("[data-test-id=city] input").setValue(dataGenerator.getCity());
+        $("[data-test-id=city] input").setValue(DataGenerator.Registration.generateCity());
         $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.BACK_SPACE));
-        $("[data-test-id=date] input").setValue(LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd-MM-yyyy")));
-        $("[data-test-id=name] input").setValue(dataGenerator.getName());
-        $("[data-test-id=phone] input").setValue(dataGenerator.getPhoneNumber());
+        $("[data-test-id=date] input").setValue(DataGenerator.Registration.generateFirstDateOfMeeting());
+        $("[data-test-id=name] input").setValue(DataGenerator.Registration.generateByFakerName("ru"));
+        $("[data-test-id=phone] input").setValue(DataGenerator.Registration.generateByFakerPhone("ru"));
         $("[data-test-id=agreement]").click();
         $(byText("Запланировать")).click();
-        $("[data-test-id=success-notification]").waitUntil(text("Встреча успешно запланирована на " + LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd-MM-yyyy"))), 15000).shouldBe(visible);
-        $("[data-test-id=date] input").setValue(LocalDate.now().plusDays(4).format(DateTimeFormatter.ofPattern("dd-MM-yyyy")));
+        $("[data-test-id=success-notification] .notification__content").waitUntil(visible, 15000).shouldHave(text("Встреча успешно запланирована на " + DataGenerator.Registration.generateFirstDateOfMeeting()));
+        $("[data-test-id=date] input").setValue(DataGenerator.Registration.generateSecondDateOfMeeting());
         $(byText("Запланировать")).click();
         $(byText("Перепланировать")).click();
-        $("[data-test-id=success-notification]").waitUntil(text("Встреча успешно запланирована на " + LocalDate.now().plusDays(4).format(DateTimeFormatter.ofPattern("dd-MM-yyyy"))), 15000).shouldBe(visible);
+        $("[data-test-id=success-notification] .notification__content").waitUntil(visible, 15000).shouldHave(text("Встреча успешно запланирована на " + DataGenerator.Registration.generateSecondDateOfMeeting()));
     }
 
 }
-
