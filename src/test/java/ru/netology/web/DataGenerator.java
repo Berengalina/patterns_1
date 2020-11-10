aecf3e7db7be6ecfe49226975fc491cb5404889f
Anna Belousova <annabelousovva@gmail.com>
2020-11-10 23:07:07 +0300

    Update Tests

--- src/test/java/ru/netology/web/DataGenerator.java
+++ src/test/java/ru/netology/web/DataGenerator.java
@@ -13,20 +13,20 @@ public class DataGenerator {
     private DataGenerator() {
     }
 
-        public static String generateByFakerName(String locale) {
-            Faker faker = new Faker(new Locale("ru"));
-            return new String(
-                    faker.name().fullName()
-            );
-        }
+    public static String generateByFakerName(String locale) {
+        Faker faker = new Faker(new Locale("ru"));
+        return new String(
+                faker.name().fullName()
+        );
+    }
 
 
-        public static String generateByFakerPhone(String locale) {
-            Faker faker = new Faker(new Locale("ru"));
-            return new String(
-                    faker.phoneNumber().phoneNumber()
-            );
-        }
+    public static String generateByFakerPhone(String locale) {
+        Faker faker = new Faker(new Locale("ru"));
+        return new String(
+                faker.phoneNumber().phoneNumber()
+        );
+    }
 
     public static String generateByFakerNameForeign(String locale) {
         Faker faker = new Faker(new Locale("es"));
@@ -35,31 +35,31 @@ public class DataGenerator {
         );
     }
 
-        public static String generateFirstDateOfMeeting() {
-            String firstDate = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-            return firstDate;
-        }
+    public static String generateFirstDateOfMeeting() {
+        String firstDate = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        return firstDate;
+    }
 
 
-        public static String generateSecondDateOfMeeting() {
-            String secondDate = LocalDate.now().plusDays(4).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-            return secondDate;
-        }
+    public static String generateSecondDateOfMeeting() {
+        String secondDate = LocalDate.now().plusDays(4).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        return secondDate;
+    }
 
-        public static String setNameWithSymbols() {
-            String name = "Подъёмова Любовь-Майя";
-            return name;
-
-        }
-
-        public static String generateCity() {
-            String[] adminCities = new String[]{"Москва", "Санкт-Петербург", "Калуга", "Владимир", "Краснодар", "Майкоп", "Уфа", "Пермь",
-                    "Владивосток", "Рязань", "Самара", "Курск", "Брянск", "Казань", "Хабаровск", "Волгоград", "Иваново", "Вологда",
-                    "Чита", "Барнаул", "Иркутск", "Якутск", "Улан-Удэ", "Петрозаводск", "Липецк"};
-            Random generator = new Random();
-            int randomIndex = generator.nextInt(adminCities.length);
-            return adminCities[randomIndex];
-        }
+    public static String setNameWithSymbols() {
+        String name = "Подъёмова Любовь-Майя";
+        return name;
 
     }
+
+    public static String generateCity() {
+        String[] adminCities = new String[]{"Москва", "Санкт-Петербург", "Калуга", "Владимир", "Краснодар", "Майкоп", "Уфа", "Пермь",
+                "Владивосток", "Рязань", "Самара", "Курск", "Брянск", "Казань", "Хабаровск", "Волгоград", "Иваново", "Вологда",
+                "Чита", "Барнаул", "Иркутск", "Якутск", "Улан-Удэ", "Петрозаводск", "Липецк"};
+        Random generator = new Random();
+        int randomIndex = generator.nextInt(adminCities.length);
+        return adminCities[randomIndex];
+    }
+
+}
 
